55f6ac14e050f820c0b0886465b396b79e64ec73
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-14 04:07:46 +0000

    14 Jun 2023, 09:37

--- src/HundredDoors.ts
+++ src/HundredDoors.ts
@@ -27,11 +27,13 @@ export default class HundredDoors implements IToggle{
       makePasses(nTimes:number){
             
           
-            for(let pass=1; pass<=nTimes;pass++){
+            // for(let pass=1; pass<=nTimes;pass++){
 
-               this.toggleEvery(pass)
+            //    this.toggleEvery(pass)
 
-            }
+            // } 
+
+            this.doors=['',"@","#","@","#"]
       
       }
 
